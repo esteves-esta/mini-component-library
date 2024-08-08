47326689ce49be8b053b441ad5e6eedc9ffc69f9
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-08-08 11:42:00 -0300

    feat: changes to progress component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,64 +8,51 @@ import VisuallyHidden from '../VisuallyHidden';
 const ProgressBar = ({ value, size }) => {
   const stylesBase = {
     base: {
-      "--value": `${value}% 100%`,
-      "--right-border-radius": `${value > 98 ? 4 : 0}px`,
+      "--right-border-radius": `${value > 99 ? 4 : 0}px`,
     },
-    small: {
-      "--height": `8px`
-    },
+    small: { "--height": `8px` },
     medium: { "--height": `12px` },
     large: { "--height": `16px` },
   };
 
   const styles = { ...stylesBase['base'], ...stylesBase[size] };
 
-  let ProgressWrapper;
-  if (size === 'large') ProgressWrapper = Wrapper;
-  else ProgressWrapper = React.Fragment;
-
   let label = value === 100 ? 'Completed' : `${value}%`;
 
   return (
-    <ProgressWrapper>
-      <Progress style={styles} value={value} type="range" min="0" max="100" />
+    <>
+      <Progress style={styles} size={size} value={value} min="0" max="100" />
       <VisuallyHidden>{label}</VisuallyHidden>
-    </ProgressWrapper>
-  );
-
-  // return (
-  //   <progress ref={progressBarRef} value={progressValue} max="1" />
-  // )
+    </>
+  )
 };
 
 export default ProgressBar;
 
-const Wrapper = styled.div`
-  background: ${COLORS.transparentGray15};
-  padding: 4px;
-  border-radius: 8px;
-  
-  /* offset-x | offset-y | blur-radius | color */
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  `
-
-const Progress = styled.input`
-// https://nikitahl.com/style-range-input-css
-  -webkit-appearance: none;
-  margin-right: 15px;
-  width: 99%;
+const Progress = styled.progress`
+  width: 100%;
   height: var(--height);
-  background: transparent;
-  border-radius: 4px;
-  border-top-right-radius: var(--right-border-radius);
-  border-bottom-right-radius: var(--right-border-radius);
-
-  background-image: linear-gradient(${COLORS.primary}, ${COLORS.primary});
-  background-size: var(--value);
-  background-repeat: no-repeat;
-  transition: background .5s ease-in-out;
+  border: none;
+  border-radius: 6px;
+  appearance: none;
   
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-  } 
+  &::-webkit-progress-bar {
+    appearance: none;
+    background: ${COLORS.transparentGray15};
+    padding: ${p => p.size === 'large' ? 4 : 0}px;
+    border-radius: 8px;
+    height: var(--height);
+    /* offset-x | offset-y | blur-radius | color */
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  }
+
+  &::-webkit-progress-value {
+    appearance: none;
+    background-color: ${COLORS.primary};
+    border-radius: 4px;
+    border-top-right-radius: var(--right-border-radius);
+    border-bottom-right-radius: var(--right-border-radius);
+    height: var(--height);
+    transition: all .5s ease-in-out;
+  }
 `
