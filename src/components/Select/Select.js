e4541eb08e20aed55420b79076dcc0bdd6f41b4f
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-08-08 16:30:57 -0300

    feat: select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,15 +4,54 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
+  return (<Wrapper>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    {displayedValue}
+    <SelectElem value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectElem>
+    <Arrow id="chevron-down" size={12} strokeWidth={2} />
+  </Wrapper>
   );
 };
 
 export default Select;
+
+const SelectElem = styled.select`
+  appearance: none;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  position: absolute;
+`
+const Wrapper = styled.label`
+  position: relative;
+  font-family: Roboto, 'sans-serif';
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  background: ${COLORS.transparentGray15} ;
+  padding: 12px 16px;
+  border-radius: 8px;
+  gap: 24px;
+  color: ${COLORS.gray700};
+
+  &:focus-within {
+    outline: ;
+  }
+
+  &:hover{
+    color: ${COLORS.black};
+  }
+`
+
+const Arrow = styled(Icon)`
+  color: inherit;
+`
