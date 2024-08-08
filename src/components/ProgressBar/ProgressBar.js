36e2bedaaa03b518643550d03478a1388e86a72a
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-08-08 11:54:49 -0300

    feat: changes to progress component - made with div

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,53 +6,41 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
+
   const stylesBase = {
-    base: {
-      "--right-border-radius": `${value > 99 ? 4 : 0}px`,
-    },
     small: { "--height": `8px` },
     medium: { "--height": `12px` },
     large: { "--height": `16px` },
   };
 
-  const styles = { ...stylesBase['base'], ...stylesBase[size] };
-
   let label = value === 100 ? 'Completed' : `${value}%`;
 
   return (
-    <>
-      <Progress style={styles} size={size} value={value} min="0" max="100" />
+    <Wrapper size={size} role="progressbar" aria-valuenow={value}>
+      <Value style={stylesBase[size]} value={value} />
       <VisuallyHidden>{label}</VisuallyHidden>
-    </>
+    </Wrapper>
   )
 };
 
 export default ProgressBar;
 
-const Progress = styled.progress`
-  width: 100%;
+const Wrapper = styled.div`
   height: var(--height);
-  border: none;
-  border-radius: 6px;
-  appearance: none;
-  
-  &::-webkit-progress-bar {
-    appearance: none;
-    background: ${COLORS.transparentGray15};
-    padding: ${p => p.size === 'large' ? 4 : 0}px;
-    border-radius: 8px;
-    height: var(--height);
-    /* offset-x | offset-y | blur-radius | color */
-    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  }
+  padding: ${p => p.size === 'large' ? 4 : 0}px;
+  width: 100%;
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  /* offset-x | offset-y | blur-radius | color */
+`
 
-  &::-webkit-progress-value {
-    appearance: none;
-    background-color: ${COLORS.primary};
-    border-radius: 4px;
-    border-top-right-radius: var(--right-border-radius);
-    border-bottom-right-radius: var(--right-border-radius);
-    height: var(--height);
-    transition: all .5s ease-in-out;
-  }
+const Value = styled.div`
+  background-color: ${COLORS.primary};
+  height: var(--height);
+  width: ${p => p.value}%;
+  border-radius: 4px 0px 0px 4px; 
+  border-top-right-radius: ${p => p.value > 99 && 4}px;
+  border-bottom-right-radius: ${p => p.value > 99 && 4}px;
+  transition: all .5s ease-in-out;
 `
