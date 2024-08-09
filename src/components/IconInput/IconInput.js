4730e0d382c34797f42368f8095053c5fe85a5a0
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-08-09 12:05:48 -0300

    feat: icon input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,23 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const sizes = {
+  small: {
+    '--font-size': `${14 / 16}rem`,
+    '--border-width': `${1}px`,
+    '--gap': `${8}px`,
+    'icon': 16,
+    'icon-stroke': 1,
+  },
+  large: {
+    '--font-size': `${18 / 16}rem`,
+    '--border-width': `${2}px`,
+    '--gap': `${12}px`,
+    'icon': 24,
+    'icon-stroke': 2,
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +30,60 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = sizes[size]
+  if (!sizes[size]) throw new Error('wrong size !');
+
+  return <Wrapper style={styles} width={`${width}px`}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper id={icon || "search"} size={styles.icon} strokeWidth={styles["icon-stroke"]} />
+    <Input style={styles} placeholder={placeholder} />
+  </Wrapper>;
 };
 
 export default IconInput;
+
+const IconWrapper = styled(Icon)`
+ color: inherit;
+`;
+
+const Input = styled.input`
+  font-family: Roboto, 'sans-serif';
+ 
+  appearance: none;
+  border: none;
+  outline: none;
+  width: 100%;
+  font-size:var(--font-size);
+  font-weight: 700;
+  color: inherit;
+
+  &::placeholder{
+   color: ${COLORS.gray500};
+   font-weight: 400;
+  }
+`;
+
+
+const Wrapper = styled.label`
+  display: flex;
+  align-items: center;
+  gap: var(--gap);
+  width: ${p => p.width};
+  border-color: ${COLORS.black};
+  border-bottom-width: var(--border-width);
+  border-style: solid;
+  padding-bottom: 4px;
+  line-height: 1.005;
+  color: ${COLORS.gray700};
+  transition: color .5s ease-in-out;
+  
+  &:focus-within {
+    outline: 2px auto -webkit-focus-ring-color;
+    outline-offset: 4px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
