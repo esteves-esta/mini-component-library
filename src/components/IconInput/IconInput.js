e385b16385f629c4fdc033cef3baddc483fcbd1d
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-08-12 15:55:34 -0300

    refactor: after watching the video

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,6 +11,7 @@ const sizes = {
     '--font-size': `${14 / 16}rem`,
     '--border-width': `${1}px`,
     '--gap': `${8}px`,
+    '--height': `${24 / 16}rem`,
     'icon': 16,
     'icon-stroke': 1,
   },
@@ -18,6 +19,7 @@ const sizes = {
     '--font-size': `${18 / 16}rem`,
     '--border-width': `${2}px`,
     '--gap': `${12}px`,
+    '--height': `${36 / 16}rem`,
     'icon': 24,
     'icon-stroke': 2,
   }
@@ -29,6 +31,7 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  ...delegated
 }) => {
   const styles = sizes[size]
   if (!sizes[size]) throw new Error('wrong size !');
@@ -36,7 +39,7 @@ const IconInput = ({
   return <Wrapper style={styles} width={`${width}px`}>
     <VisuallyHidden>{label}</VisuallyHidden>
     <IconWrapper id={icon || "search"} size={styles.icon} strokeWidth={styles["icon-stroke"]} />
-    <Input style={styles} placeholder={placeholder} />
+    <TextInput style={styles} placeholder={placeholder} {...delegated} />
   </Wrapper>;
 };
 
@@ -46,9 +49,9 @@ const IconWrapper = styled(Icon)`
  color: inherit;
 `;
 
-const Input = styled.input`
+const TextInput = styled.input`
   font-family: Roboto, 'sans-serif';
- 
+  
   appearance: none;
   border: none;
   outline: none;
@@ -67,6 +70,7 @@ const Input = styled.input`
 const Wrapper = styled.label`
   display: flex;
   align-items: center;
+  height: var(--height);
   gap: var(--gap);
   width: ${p => p.width};
   border-color: ${COLORS.black};
